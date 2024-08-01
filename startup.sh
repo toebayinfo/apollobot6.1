2c07df3a13e38376eccba178e8129c78e94bd215
toebayinfo@gmail.com <toebayinfo@gmail.com>
2024-08-02 00:58:52 +0300

    updated bot.py to update asyncio2

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/site/wwwroot
-python -m venv antenv
+python3 -m venv antenv
 source antenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-gunicorn --bind 0.0.0.0:8000 app:init_func
+gunicorn --bind=0.0.0.0:8000 app:init_func
