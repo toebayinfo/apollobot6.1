b78ce238898bbce798b7d8a1456121c4877909f2
toebayinfo@gmail.com <toebayinfo@gmail.com>
2024-08-02 04:50:05 +0300

    fixing errors in github actions6

--- config.py
+++ config.py
@@ -19,5 +19,4 @@ class CONFIG:
     
     WEBSITES_PORT = os.getenv("WEBSITES_PORT", 8000)  # Default to 8000 if not set
     PORT = os.getenv("PORT", 8000)  # Default to 8000 if not set
-    
-    
+
