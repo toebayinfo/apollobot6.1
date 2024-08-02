d78fb56e64f8aa943ded1878a343a56df514ecb5
toebayinfo@gmail.com <toebayinfo@gmail.com>
2024-08-02 22:44:38 +0300

    Update deploy.sh to start application

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # Install dependencies without cache
 pip install -r requirements.txt --no-cache-dir
 
+pip install --upgrade pip
+
 # Any other deployment steps can go here, for example:
 # python manage.py migrate
 # python manage.py collectstatic --noinput
